dc3014f1dcfb6d0a8b7721c4c5bd4d3670afbdb2
Wayne Hsu <71208879+ThatMrWayne@users.noreply.github.com>
2025-01-08 17:30:48 +0800

    WIP: update run django sh file

--- docker/scripts/run_django_dev.sh
+++ docker/scripts/run_django_dev.sh
@@ -3,5 +3,8 @@
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput
 
+echo "run migration..."
+python3 manage.py migrate
+
 echo "Starting server..."
 python3 manage.py runserver 0.0.0.0:8000
