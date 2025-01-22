18d03b8724e7b411e9fb9baef7cc4be984d6dae6
Wayne Hsu <71208879+ThatMrWayne@users.noreply.github.com>
2025-01-22 14:54:56 +0800

    DEV: test deploy with codeDeploy

--- deploy_scripts/stop.sh
+++ deploy_scripts/stop.sh
@@ -2,4 +2,4 @@
 cd /home/ubuntu/production
 echo "Stop current containers"
 sudo -E docker compose -f ./docker-compose-prod-codedeploy.yml down -v
-echo "Stop current containers do
+echo "Stop current containers done"
