52cf095ef799e31e0dc7f51c668d1b6545b8ce97
Wayne Hsu <71208879+ThatMrWayne@users.noreply.github.com>
2025-01-22 17:54:38 +0800

    DEV: test deploy with codeDeploy

--- deploy_scripts/setup.sh
+++ deploy_scripts/setup.sh
@@ -5,7 +5,7 @@ docker image rm 298325099374.dkr.ecr.us-east-1.amazonaws.com/macroseat-postgres:
 docker image rm 298325099374.dkr.ecr.us-east-1.amazonaws.com/macroseat-nginx:latest
 docker image rm 298325099374.dkr.ecr.us-east-1.amazonaws.com/macroseat-django:latest
 echo "login ECR"
-aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 298325099374.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 298325099374.dkr.ecr.us-east-1.amazonaws.com
 echo "Pull latest images from ECR"
 docker image pull 298325099374.dkr.ecr.us-east-1.amazonaws.com/macroseat-postgres:latest
 docker image pull 298325099374.dkr.ecr.us-east-1.amazonaws.com/macroseat-django:latest
